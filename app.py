3466abcfd77f1ddaf02ef0dfe92a319d5aaf7868
Divyansh853 <90828968+Divyansh853@users.noreply.github.com>
2023-12-14 08:37:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import cv2
+import cv2 as cv
+import numpy as np
+
+BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+              "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+              "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+              "LEye": 15, "REar": 16, "LEar": 17, "Background": 18}
+
+POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+              ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+              ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+              ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+              ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"]]
+
+width = 368
+height = 368
+inWidth = width
+inHeight = height
+
+net = cv.dnn.readNetFromTensorflow(r"C:\Users\Divyansh\Desktop\Computer Vision Project\graph_opt.pb")
+thr = 0.2
+
+def poseDetector(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :19, :, :]
+
+    assert(len(BODY_PARTS) == out.shape[1])
+
+    points = []
+    for i in range(len(BODY_PARTS)):
+        heatMap = out[0, i, :, :]
+
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / out.shape[3]
+        y = (frameHeight * point[1]) / out.shape[2]
+        points.append((int(x), int(y)) if conf > thr else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert(partFrom in BODY_PARTS)
+        assert(partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (255, 0, 0), 3)
+            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+
+    return frame
+
+desired_width = 1280
+desired_height = 720
+
+def main():
+    st.title("Pose Detection with Streamlit")
+
+    cap = cv2.VideoCapture(0)
+    cap.set(cv.CAP_PROP_FRAME_WIDTH, desired_width)
+    cap.set(cv.CAP_PROP_FRAME_HEIGHT, desired_height)
+
+    stop_button = st.button("Stop", key="stop_button")
+
+    while not stop_button:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        output = poseDetector(frame)
+
+        st.image(output, channels="BGR", use_column_width=True)
+
+    cap.release()
+
+if __name__ == '__main__':
+    main()
